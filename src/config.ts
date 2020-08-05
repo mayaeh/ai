f3d0830798a84b8d68600b0ca668ab1952c12408
Maya Minatsuki <mayaeh@marimo-net.org>
2020-08-05 15:49:05 +0900

    Revert "Modify configuration from mazeAutoPostEnabled to mazeEnabled."
    This reverts commit 796bab313967bbc357941e866b7590d72e75f62a.

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ type Config = {
 	chartEnabled: boolean;
 	welcomeEnabled: boolean;
 	serverMonitoring: boolean;
-	mazeEnabled: boolean;
+	mazeAutoPostEnabled: boolean;
 	versionCheckAutoPostEnabled: boolean;
 	mecab?: string;
 	mecabDic?: string;
